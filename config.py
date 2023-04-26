7bab59a84e4861e39f3aac033ecfce150ad1fd22
unknown <119958500@qq.com>
2023-04-26 19:54:31 +0800

    f

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 MYSQL_USER = 'root'
 MYSQL_HOST = 'localhost'
 MYSQL_PORT = 3306
-MYSQL_PASSWORD = '20200622$Eeoa'
+MYSQL_PASSWORD = ''
 
 DATABASE = 'my_statistic'
